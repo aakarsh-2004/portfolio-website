68974017af068083e7565c0e3c616865e532df06
sp4-rta <aakarshbeohar2004@gmail.com>
2023-06-09 22:55:43 +0530

    Fixed twitter icon and portfolio section

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -5,8 +5,13 @@ import Sidebar from '../../img/sidebar.png';
 import Ecommerce from '../../img/ecommerce.png';
 import Hoc from '../../img/hoc.png';
 import MusicApp from '../../img/musicapp.png';
+import DrumKit from '../../img/drum-kit.png';
+import DiceGame from '../../img/dice-game.png';
+import WeatherApp from '../../img/weather-app.png';
+import BlogWebsite from '../../img/blog.png';
 import 'swiper/css';
 import { themeContext } from '../../context';
+
 
 export default function Portfolio() {
     const theme = useContext(themeContext);
@@ -20,16 +25,16 @@ export default function Portfolio() {
             <Swiper spaceBetween={30} slidesPerView={3} grabCursor = {true} className='portfolio-slider'
             >
                 <SwiperSlide>
-                    <img src= {Sidebar} alt="" />
+                    <a href="https://weatherapp-fk6n.onrender.com/" target='_blank'><img src= {WeatherApp} alt="" /></a>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src= {Ecommerce} alt="" />
+                    <a href="https://aakarsh-2004.github.io/drum-kit/" target='_blank'><img src= {DrumKit} alt="" /></a>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src= {MusicApp} alt="" />
+                    <a href="https://aakarsh-2004.github.io/dice-game/" target='_blank'><img src= {DiceGame} alt="" /></a>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <img src= {Hoc} alt="" />
+                    <a href="" target='_blank'><img src= {BlogWebsite} alt="" /></a>
                 </SwiperSlide>
             </Swiper>
         </div>
